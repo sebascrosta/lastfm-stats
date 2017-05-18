f4bb588f6aa663075b06a00104fff438d1f5805f
Sebas <sebas.crosta@gmail.com>
2017-05-17 23:26:20 -0300

    Andando todo

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Muestra a todos los artistas con más de 10 scrobbles.
+
 function showArtists (){
   var results = {};
 
@@ -13,7 +15,7 @@ function showArtists (){
   var text = "";
   for(i = 0; i < keys.length; i++){
     if(results[keys[i]] >= 10){
-      text += ('<a href="" onclick="sendToInput(event)">' + keys[i]+ '</a>' + " - " + results[keys[i]] + '<br>')
+      text += ('<a href="" onclick="sendToArtistInput(event)">' + keys[i]+ '</a>' + " - " + results[keys[i]] + '<br>')
     }
   }
 
@@ -21,31 +23,13 @@ function showArtists (){
 
 }
 
+//Si buscás una canción, te devuelve cuántas veces y cuando la escuchaste.
 
-function showArtistAndQuantity (){
-  var results = document.getElementById('result');
-  var artists = [];
-
-  for(var i = 0; i < json.length; i++){
-    if(artists[json[i].artist]){
-      artists[json[i].artist] = artists[json[i].artist] + 1;
-    } else {
-      artists[json[i].artist] = 1;
-    }
-  }
-
-  var keys = Object.keys(artists);
-
-  for(i = 0; i < keys.length -1 ; i++){
-    var newLine = (keys[i] + " - " + artists[keys[i]] + '<br>');
-      results.innerHTML += newLine
-  }
-}
-
-function filterBySong(song){
+function filterBySong(){
   var quantity = 0;
   var date_listened;
-  var final_result = "La canción buscada es " + song + ":\n"
+  var song = document.getElementsByName('song-input')[0].value;
+  var final_result = "";
 
   for (var i = 0; i < json.length; i++){
     if(json[i].song.toLowerCase() == song.toLowerCase()){
@@ -54,16 +38,17 @@ function filterBySong(song){
       quantity++;
     }
   }
-
-    final_result += "Cantidad: ";
-    final_result += quantity;
-
-    document.getElementById('result').innerHTML = final_result;
+  
+  var cabecera = "La canción buscada es " + song + " y la escuché " + quantity + " veces\n"
+    
+    document.getElementById('result').innerHTML = (cabecera + final_result);
 }
+
+//Muestra todas las canciones de un artist (El el input) a y la cantidad de scrobbles de cada una
 
 function findArtistSongs (){
   
-  var artist = document.getElementsByName('artist')[0].value;
+  var artist = document.getElementsByName('artist-input')[0].value;
   var results = {};
   var quantity = 0;
 
@@ -83,14 +68,108 @@ function findArtistSongs (){
   var newline;
 
   for(i = 0; i < keys.length -1 ; i++){
-    text +=  (keys[i] + " - " + results[keys[i]] + '<br>')
+      text += ('<a href="" onclick="sendToSongInput(event)">' + keys[i]+ '</a>' + " - " + results[keys[i]] + '<br>')
   }
 
   document.getElementById('result').innerHTML = text;
 
 }
 
-function sendToInput(e){
+//Muestra los 100 cds más escuchados
+
+function mostListenedAlbums (){
+  var results = [];
+
+  for(var i = 0; i < json.length; i++){
+    if(results[json[i].album]){
+      results[json[i].album] ++;
+    }else{
+      results[json[i].album] = 1;
+    }
+  }
+
+  var keys = Object.keys(results).sort(function(a,b){return results[b]-results[a]});
+  var text = "";
+  for(i = 0; i < 100; i++){
+    if(keys[i]){
+      text += ('<a href="" onclick="sendToAlbumInput(event)">' + keys[i]+ '</a>' + " - " + results[keys[i]] + '<br>')
+    }
+  }
+
+  document.getElementById('result').innerHTML = text;
+
+}
+
+//Muestra las 250 canciones maś escuchadas
+
+
+function mostListenedSongs (){
+  var results = {};
+
+  for(var i = 0; i < json.length; i++){
+    if(results[json[i].song]){
+      results[json[i].song] ++;
+    }else{
+      results[json[i].song] = 1;
+    }
+  }
+
+  var keys = Object.keys(results).sort(function(a,b){return results[b]-results[a]});
+  var text = "";
+  for(i = 0; i < 100; i++){
+    if(keys[i]){
+      text += ('<a href="" onclick="sendToSongInput(event)">' + keys[i]+ '</a>' + " - " + results[keys[i]] + '<br>')
+    }
+  }
+
+  document.getElementById('result').innerHTML = text;
+
+}
+
+//Muestra los scrobbles de cada cancion del disco que está en el input
+
+function findAlbumSongs(){
+    var results = [];
+    var album = document.getElementsByName('album-input')[0].value.toLowerCase();
+
+  for(var i = 0; i < json.length; i++){
+    if(json[i].album.toLowerCase() == album){
+      if(results[json[i].song]){
+        results[json[i].song] ++;
+      }else{
+        results[json[i].song] = 1;
+      }
+    }
+  }
+
+  var keys = Object.keys(results).sort(function(a,b){return results[b]-results[a]});
+  var text = "";
+  for(i = 0; i < 100; i++){
+    if(keys[i]){
+      text += ('<a href="" onclick="sendToSongInput(event)">' + keys[i]+ '</a>' + " - " + results[keys[i]] + '<br>')
+    }
+  }
+
+  document.getElementById('result').innerHTML = text;
+
+}
+
+// Si clickea en un link, lo manda a su input y ejecuta la función correspondiente para ver más info.
+
+function sendToAlbumInput(e){
   e.preventDefault();
-  document.getElementsByName('artist')[0].value = e.currentTarget.text
+  document.getElementsByName('album-input')[0].value = e.currentTarget.text;
+  findAlbumSongs();
+}
+
+function sendToSongInput(e){
+  e.preventDefault();
+  document.getElementsByName('song-input')[0].value = e.currentTarget.text;
+  filterBySong();
+}
+
+function sendToArtistInput(e){
+  e.preventDefault();
+  document.getElementsByName('artist-input')[0].value = e.currentTarget.text;
+  findArtistSongs();
 }
